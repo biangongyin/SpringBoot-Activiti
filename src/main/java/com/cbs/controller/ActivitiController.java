5a69f8afdb0523af3fcd0d828d6484c828349bde
biangongyin <1071268432@qq.com>
2020-10-21 09:46:35 +0800

    去除多余的引用包
    Signed-off-by: biangongyin <1071268432@qq.com>

--- src/main/java/com/cbs/controller/ActivitiController.java
+++ src/main/java/com/cbs/controller/ActivitiController.java
@@ -39,7 +39,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.alibaba.fastjson.JSON;
 import com.cbs.pagemodel.DataGrid;
 import com.cbs.pagemodel.HistoryProcess;
 import com.cbs.pagemodel.LeaveTask;
--- src/main/java/com/cbs/controller/PurchaseController.java
+++ src/main/java/com/cbs/controller/PurchaseController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.cbs.pagemodel.DataGrid;
 import com.cbs.pagemodel.HistoryProcess;
 import com.cbs.pagemodel.MSG;
